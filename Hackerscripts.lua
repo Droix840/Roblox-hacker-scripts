7c7e5c90f440aee93035efec9f5d69a68f100d57
Droix840 <116375935+Droix840@users.noreply.github.com>
2026-05-11 21:27:46 +0200

    Refactor color assignment and improve animations

--- Hackerscripts.lua
+++ Hackerscripts.lua
@@ -4,8 +4,17 @@ local players = game:GetService("Players")
 local siposa = players.siposatti093
 local char = siposa.Character
 local hum = char.Humanoid
-local red = Color3.fromRGB(255,0,0)
-local black = Color3.fromRGB(0,0,0)
+local colors = [
+red = Color3.fromRGB(255,0,0)
+orange = Color3.fromRGB(255,128,0)
+yellow = Color3.fromRGB(255,204,0)
+green = Color3.fromRGB(0,0,255)
+lightblue = Color3.fromRGB(0, 255, 247)
+blue = Color3.fromRGB(0, 174, 255)
+darkblue = Color3.fromRGB(0, 94, 255)
+purple = Color3.fromRGB(149, 0, 255)
+pink = Color3.fromRGB(234, 0, 255)
+]
 local rlarm = char.RightLowerArm
 local ruarm = char.RightUpperArm
 local llarm = char.LeftLowerArm
@@ -33,37 +42,39 @@ end
 end
 --function
 hum.WalkSpeed = 130
-hum.JumpPower = 120
+hum.JumpPower = 300
 char.HumanoidRootPart.Transparency = 0.7
 char.HumanoidRootPart.Touched:Connect(function(hit)
-hit.Parent.Humanoid.Health = 0
-for _,chilld in pairs(hit.Parent:GetChildren()) do
-if child:IsA("BasePart") then
-child.Color = red
-child.Material = Enum.Material.Metal
-end
-if child:IsA("Clothing") or child == BodyColors then
-child:Destroy()
-end
-end
+    for _,chilld in pairs(hit.Parent:GetChildren()) do
+        if child:IsA("BasePart") then
+            child.Color = red
+            child.Material = Enum.Material.Metal
+        end
+        if child:IsA("Clothing") or child == BodyColors then
+            child:Destroy()
+        end
+        hit.Parent.Humanoid.Health = 0
+    end
 end)
 --animation
 while true do
-rlarm = 
-ruarm = 
-llarm = 
-ularm = 
-rhand = 
-lhand = 
-head = 
-utorso = 
-ltorso = 
-ruleg = 
-rlleg = 
-rfoot = 
-luleg = 
-llleg = 
-lfoot = 
+    for _,child in pairs(colors)
+        rlarm = child
+        ruarm = child
+        llarm = child
+        ularm = child
+        rhand = child
+        lhand = child
+        head = child
+        utorso = child
+        ltorso = child
+        ruleg = child
+        rlleg = child
+        rfoot = child
+        luleg = child
+        llleg = child
+        lfoot = child
+        task.wait(0.25)
 end
 --[[
 while true do
@@ -83,5 +94,3 @@ hand.Color = red
 task.wait(3)
 end
 ]]
-
-
