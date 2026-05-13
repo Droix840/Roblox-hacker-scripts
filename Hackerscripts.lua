de68c9d456a38f1e35b71bfbee0ea0f21c0adbe0
Droix840 <116375935+Droix840@users.noreply.github.com>
2026-05-13 20:47:02 +0200

    Update Hackerscripts.lua

--- Hackerscripts.lua
+++ Hackerscripts.lua
@@ -1,9 +1,9 @@
 --Look
 --varibles
 local players = game:GetService("Players")
-local siposa = players:WaitForChild("siposatti093")
+local siposa = players:WaitForChild("siposatti093") or siposa.CharacterAdded:Wait()
 local char = siposa.Character
-local hum = char.Humanoid
+local hum = char:WaitForChild("Humanoid")
 local colors = { 
 Color3.fromRGB(255,0,0), 
 Color3.fromRGB(255,128,0), 
@@ -32,52 +32,74 @@ local luleg = char.LeftUpperLeg
 local llleg = char.LeftLowerLeg 
 local lfoot = char.LeftFoot
 ]]--
-
+--METHODS
+local function coloring(givedChar)
+    task.spawn(function()
+        while true do
+            for _,color in pairs(colors) do
+                for _,part in pairs(givedChar:GetChildren()) do
+                    if part:IsA("BasePart") then
+                    part.Color = color
+                    end
+                end
+                task.wait(0.25)
+            end 
+        end
+    end)
+end
+local function kill(targetPart)
+    targetPart.Parent.Humanoid.Health = 0
+end
+--GUI
+local sgui = Instance.new("ScreenGUI", siposa.PlayerGUI)
+local frame = Instance.new("Frame", sgui)
+local textbutton = Instance.new("TextButton", frame)
+textbutton.Text = "Click for killing"
+textbutton.TextSize = 30
+frame.AnchorPoint = Vector2.new(0.5, 0.5)
+frame.Position = UDim2.new(0.2, 0, 0.5, 0)
+local switch = false
+task.spawn(function()
+    textbutton.MouseButton1Clicked:Connect(function()
+        switch = not switch
+        return switch
+    end)
+end)
 --mainpart 
 for _,child in pairs(char:GetChildren()) do
-if child:IsA("BasePart") then
-child.Color =  colors[1]
-child.Material = Enum.Material.Neon
-end  
-if child:IsA("Clothing") or child:IsA("BodyColors") then 
-child:Destroy()
-end 
+    if child:IsA("BasePart") then
+        child.Color =  colors[1]
+        child.Material = Enum.Material.Neon
+    end  
+        if child:IsA("Clothing") or child:IsA("BodyColors") or child:IsA("Accesory") then 
+        child:Destroy()
+    end 
 end
- 
 --function varibles
 hum.WalkSpeed = 130
-hum.JumpPower = 300 
+hum.JumpPower = 420
 char.HumanoidRootPart.Transparency = 0.7
 --function 
 char.HumanoidRootPart.Touched:Connect(function(hit)
-local targetChar = hit.Parent
-local targetHum = targetChar:WaitForChild("Humanoid")
-if targetHum and targetChar ~= char then
-    for _,chilld in pairs(targetChar:GetChildren()) do 
-        if child:IsA("BasePart") then
-            child.Color = colors[1] 
-            child.Material = Enum.Material.Metal 
-elseif child:IsA("Clothing") or child:IsA("BodyColors") then 
-            child:Destroy() 
-        end 
+    local targetChar = hit.Parent
+    local targetHum = targetChar:WaitForChild("Humanoid")
+    if targetHum and targetChar ~= char then
+        for _,child in pairs(targetChar:GetChildren()) do 
+            if child:IsA("BasePart") then
+                child.Color = colors[1] 
+                child.Material = Enum.Material.Metal 
+            elseif child:IsA("Clothing") or child:IsA("BodyColors") or child:IsA("Accesory") then 
+                child:Destroy() 
+            end 
+        end
+        if switch == true then
+            kill(hit)
+        end
     end
---hit.Parent.Humanoid.Health = 0
-end 
 end) 
 
 --animation 
-task.spawn(function()
-while true do
-for _,color in pairs(colors) do
-for _,part in pairs(char:GetChildren()) do
-if part:IsA("BasePart") then
-part.Color = color
-end
-end
-task.wait(0.25)
-end 
-end
-end)
+coloring(char)
 --[[ 
 while true do 
 lhand.Material = Enum.Material.Rubber 
